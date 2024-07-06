ddec3de1274242cd02419bfafffbd22f1bea8b17
banditklm <kaltoum.elmounjid123@gmail.com>
2024-07-06 17:23:51 +0100

    simple && double quotes

--- sources/minishell.h
+++ sources/minishell.h
@@ -6,7 +6,7 @@
 /*   By: kelmounj <kelmounj@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 14:14:17 by kelmounj          #+#    #+#             */
-/*   Updated: 2024/07/02 08:50:40 by kelmounj         ###   ########.fr       */
+/*   Updated: 2024/07/06 15:19:42 by kelmounj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,9 +26,15 @@ typedef struct s_token
 	struct s_token *next;
 }	t_token;
 
+typedef struct s_syntax_err
+{
+	int	bool;
+	int	index;
+}	t_syntax_err;
+
 char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_isalnum(int c);
-int		s_quote(char *line);
+// int		s_quote(char *line);
 void	parser(char *line);
 
 # endif
--- sources/parser1.c
+++ sources/parser1.c
@@ -6,113 +6,153 @@
 /*   By: kelmounj <kelmounj@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:55:39 by kelmounj          #+#    #+#             */
-/*   Updated: 2024/07/02 09:10:42 by kelmounj         ###   ########.fr       */
+/*   Updated: 2024/07/06 16:11:09 by kelmounj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	s_quote(char *line)
+t_syntax_err	s_quote(char *line)
 {
-	int	i;
-	int open;
-	int	d;
+	int				i;
+	int				d;
+	t_syntax_err	syntx_err;
 
 	i = 0;
-	open = 0;
 	d = 0;
+	syntx_err.bool = 0;
+	syntx_err.index = 0;
 	while (line[i])
 	{
 		if (line[i]	== '"')
 			d = !d;
 		if (line[i] == '\'' && d == 0)
-			open = !open;
+		{
+			syntx_err.bool = !syntx_err.bool;
+			syntx_err.index = i;
+		}
 		i++;
 	}
-	if (open == 0)
-		return (1);
-	else 
-		return (0);
+	return (syntx_err);
 }
 
-int	d_quote(char *line)
+t_syntax_err	d_quote(char *line)
 {
-	int	i;
-	int open;
-	int	s;
+	int				i;
+	int				s;
+	t_syntax_err	syntx_err;
 
 	i = 0;
-	open = 0;
 	s = 0;
+	syntx_err.bool = 0;
+	syntx_err.index = 0;
 	while (line[i])
 	{
 		if (line[i]	== '\'')
 			s = !s;
 		if (line[i] == '"' && s == 0)
-			open = !open;
+		{
+			syntx_err.bool = !syntx_err.bool;
+			syntx_err.index = i;
+		}
 		i++;
 	}
-	if (open == 0)
-		return (1);
-	else
-		return (0);
-}
-
-int	is_spaces(char *line, int *i)
-{
-	(*i) += 2;
-	while (line[*i] && (line[*i] == ' ' || line[*i] == '\t'))
-	{
-		(*i)++;
-	}
-	if (line [*i] && line[*i] != '<' && line[*i] != '>' && line[*i] != '|')
-		return (1);
-	else
-		return (0);
+	return (syntx_err);
 }
 
 
 
-int	heredoc(char *line)
+void	open_heredocs(char *line, int n)
 {
 	int	i;
-	int	s;
-	int	d;
-
+	
 	i = 0;
-	s = 0;
-	d = 0;
-	while (line[i])
+	while (i < n)
 	{
-		if (line[i]	== '"')
-			d = !d;
-		if (line[i]	== '\'')
-			s = !s;
-		if (s == 0 && d == 0 && line[i] == '<' && line[i + 1] == '<' && !is_spaces(line, &i))
-			return (0);
+		if (line[i] == '<' && line[i + 1] == '<' && line[i + 2])
+		{
+			printf("herdoc open\n");
+		}
 		i++;
-	}
-	return (1);
+	}	
 }
 
-// void	open_heredocs(char *line)
+void	issyntax_err(char *line)
+{
+	int	i;
+	t_syntax_err syntx_err1;
+	t_syntax_err syntx_err2;
+
+	syntx_err1 = s_quote(line);
+	syntx_err2 = d_quote(line);
+
+	i = 0;
+	if (syntx_err1.bool == 1)
+	{
+		open_heredocs(line, syntx_err1.index);
+		printf("syntax error '\n");
+	}
+	if (syntx_err2.bool == 1)
+	{
+		open_heredocs(line, syntx_err2.index);
+		printf("syntax error \"\n");
+	}
+}
+
+// int	is_spaces(char *line, int *i)
 // {
-	//open all heredocs
+// 	(*i) += 2;
+// 	while (line[*i] && (line[*i] == ' ' || line[*i] == '\t'))
+// 	{
+// 		(*i)++;
+// 	}
+// 	if (line [*i] && line[*i] != '<' && line[*i] != '>' && line[*i] != '|')
+// 		return (1);
+// 	else
+// 		return (0);
 // }
+
+
+// int	heredoc(char *line)
+// {
+// 	int	i;
+// 	int	s;
+// 	int	d;
+
+// 	i = 0;
+// 	s = 0;
+// 	d = 0;
+// 	while (line[i])
+// 	{
+// 		if (line[i]	== '"')
+// 			d = !d;
+// 		if (line[i]	== '\'')
+// 			s = !s;
+// 		if (s == 0 && d == 0 && line[i] == '<' && line[i + 1] == '<' && !is_spaces(line, &i))
+// 			return (0);
+// 		i++;
+// 	}
+// 	return (1);
+// }
+
+
 
 void	parser(char *line)
 {
-	if (!s_quote(line))
-		printf("syntax error\n");
-	if (!d_quote(line))
-		printf("syntax error\n");
-	if (!heredoc(line))
-		printf("syntax error\n");
+	
+	issyntax_err(line);
+	// if (!s_quote(line))
+	// 	printf("syntax error\n");
+	// if (!d_quote(line))
+	// 	printf("syntax error\n");
+	// if (!heredoc(line))
+	// 	printf("syntax error\n");
 }
-
-
 
 // int main(int ac, char **av)
 // {
-// 	printf("%d", heredoc(av[1]));
+// 	if (ac > 2)
+// 	{
+// 		printf("%d", is_heredoc(av[1]));
+// 	}
 // }
