2e13063ec6eaa3bc23c85e8adf3f80e7766a9263
Soufiane Arif <sarif@e3r1p7.1337.ma>
2024-07-20 00:03:19 +0100

    fix ft_getenv.c

--- sources/ft_getenv.c
+++ sources/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: sarif <sarif@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 20:49:53 by sarif             #+#    #+#             */
-/*   Updated: 2024/07/15 20:49:55 by sarif            ###   ########.fr       */
+/*   Updated: 2024/07/20 00:01:58 by sarif            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,21 @@
 char	*ft_getenv(char *var, t_minishell *minishell)
 {
 	char	*tmp;
+	size_t	len;
+	int		check;
 	int		i;
 
 	i = 0;
+	len = ft_strlen(var);
 	while (minishell->env[i])
 	{
 		tmp = ft_strdup(minishell, &minishell->global, minishell->env[i]);
-		if (ft_strncmp(var, minishell->env[i], ft_strlen(var)) == 0)
+		check = ft_strncmp(var, minishell->env[i], ft_strlen(var));
+		if (!check && minishell->env[i][len] == '=')
+		{
+			tmp = tmp + len + 1;
 			return (tmp);
+		}
 		free(tmp);
 		i++;
 	}
